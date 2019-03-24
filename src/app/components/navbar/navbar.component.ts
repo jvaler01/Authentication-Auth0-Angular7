d1b68814ab16bce12f3b8aeb92340dffcfdbc2af
Jorge Valero <jorgevm13@gmail.com>
2019-03-24 17:43:50 +0100

    Authentication app with Auth0 and Angular 7

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styles: []
+})
+export class NavbarComponent implements OnInit {
+
+  constructor( private auth: AuthService) {
+    auth.handleAuthentication();
+  }
+
+  ngOnInit() {
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      this.auth.renewTokens();
+    }
+  }
+
+  login() {
+    this.auth.login();
+  }
+  logout() {
+    this.auth.logout();
+  }
+}
